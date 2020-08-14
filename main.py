6ebd42f39a9e583bc327b5647bd37c7d1a727cc6
Heinrich Kuttler <hnr@fb.com>
2020-08-14 15:53:51 +0200

    Python + CMake skeleton.

---
+++ main.py
@@ -0,0 +1,10 @@
+from skel import skel
+
+
+def main():
+    s = skel.Skel(666)
+    print(s.skel())
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+import os
+import pathlib
+import subprocess
+import sys
+
+import setuptools
+from setuptools.command import build_ext
+from distutils import spawn
+
+
+class CMakeBuild(build_ext.build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        source_path = pathlib.Path(__file__).parent.resolve()
+        output_path = (
+            pathlib.Path(self.get_ext_fullpath(ext.name))
+            .parent.joinpath("skel")
+            .resolve()
+        )
+        os.makedirs(self.build_temp, exist_ok=True)
+        build_type = "Debug" if self.debug else "Release"
+
+        generator = "Ninja" if spawn.find_executable("ninja") else "Unix Makefiles"
+
+        cmake_cmd = [
+            "cmake",
+            str(source_path),
+            "-G%s" % generator,
+            "-DPYTHON_SRC_PARENT=%s" % source_path,
+            "-DPYTHON_EXECUTABLE=%s" % sys.executable,
+            "-DCMAKE_BUILD_TYPE=%s" % build_type,
+            "-DCMAKE_INSTALL_PREFIX=%s" % sys.base_prefix,
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=%s" % output_path,
+        ]
+
+        build_cmd = ["cmake", "--build", "."]
+        install_cmd = build_cmd + ["--target", "install"]
+
+        try:
+            subprocess.check_call(cmake_cmd, cwd=self.build_temp)
+            subprocess.check_call(build_cmd, cwd=self.build_temp)
+        except subprocess.CalledProcessError:
+            # Don't obscure the error with a setuptools backtrace.
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="skel",
+        version="0.0.1",
+        ext_modules=[setuptools.Extension("skel", sources=[])],
+        cmdclass={"build_ext": CMakeBuild},
+        setup_requires=["pybind11>=2.2"],
+        install_requires=["pybind11>=2.2"],
+        zip_safe=False,
+    )
